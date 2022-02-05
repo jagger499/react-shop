443dfce044d84234168eb7192e7ada6b971e39d1
jhon garcia <jagg499@gmail.com>
2022-02-04 19:37:22 -0500

    configuration in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,55 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js','.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(jsx|js)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.html$/,
-                use: {
-                    loader: 'html-loader'
-                }
-            }
-        ]
-    },
-    plugins : [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: './index.html'
-        })
-    ]
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/',
+	},
+	mode: 'development',
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader'
+					}
+				]
+			},
+			{
+				test: /\.(scss|css)$/,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader",
+				],
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+	],
+	devServer: {
+		historyApiFallback: true,
+	}
 }
